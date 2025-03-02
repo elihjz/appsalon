8e0329a655782b8ec3b5e51047f5b875dd198caf
HXHdevs <jordisonnicolas@gmail.com>
2025-03-02 01:05:23 -0600

    Agregando routing

--- public/index.php
+++ public/index.php
@@ -1,7 +1,21 @@
 <?php
+
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 $router = new Router();
+
+$router->get('/', [LoginController::class,'login']);
+$router->post('/', [LoginController::class,'login']);
+$router->get('/logout', [LoginController::class,'logout']);
+
+$router->get('/olvide',[LoginController::class,'olvide']);
+$router->post('/olvide',[LoginController::class,'olvide']);
+$router->get('/recuperar',[LoginController::class,'recuperar']);
+$router->post('/recuperar',[LoginController::class,'recuperar']);
+
+$router->get('/crear-cuenta',[LoginController::class,'crear']);
+$router->post('/crear-cuenta',[LoginController::class,'crear']);
 
 $router->comprobarRutas();
