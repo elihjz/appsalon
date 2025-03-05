291a625dcbfa164d88c7b201e6b8c8d55ab55019
HXHdevs <jordisonnicolas@gmail.com>
2025-03-05 01:06:48 -0600

    Agregando pagina olvide mi password

---
+++ views/auth/olvide-password.php
@@ -0,0 +1,21 @@
+<h1 class="nombre-pagina">Olvide mi password</h1>
+<p class="descripcion-pagina">Reestablece tu password con tu email a continuacion</p>
+
+
+<form action="formulario" action="/olvide" method="POST">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input 
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Tu correo"
+            />
+    </div>
+    <input type="submit" class="boton" value="Enviar instrucciones">
+</form>
+
+<div class="acciones">
+    <a href="/">Ya tienes una? Inicia sesion</a>
+    <a href="/crear-cuenta">Aun no tienes cuenta? Crea una.</a>
+</div>
